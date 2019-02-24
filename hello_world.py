374e0d417fc9a553425d939d2b847de03dad0506
Adel Rahimi <rahimi.adel@gmail.com>
2019-02-24 10:28:36 +0330

    initial

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+__author__ = "Adel Rahimi"
+__email__ = "Rahimi[dt]adel[at_sign]gmail[dt]com"
+
+print("Hello World!") # all the string go inside either " or '
+
+a_number: int = 12
+a_float: float = 12.0
+a_string: str = "A string"
+print("My chosen number is: "+str(a_number))
